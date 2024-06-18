c56d5c9e2237107e6294d35787ef93c9f9b9780c
enterpannet <enterpannet@gmail.com>
2024-06-18 20:10:55 +0700

    add prisma and add cors

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors'
+
+const app = express()
+const port = 3000
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('สว้สดีชาวโลก')
+})
+
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`)
+})
